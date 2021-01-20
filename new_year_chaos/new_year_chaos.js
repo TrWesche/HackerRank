42075029d6700cf5dd26841740bc6cc9bf9255dd
Trenton Wesche <TrWesche@live.com>
2021-01-20 13:37:23 -0600

    Better solution but nested loops wont run quick enough in js

--- new_year_chaos/new_year_chaos.js
+++ new_year_chaos/new_year_chaos.js
@@ -67,56 +67,83 @@
 // }
 
 
+// const newYearChaos = (q) => {
+//     let totalSwaps = 0;
+//     let tooChaotic = false;
+
+//     // Check for to chaotic O(n)
+//     let lastValue = null;
+//     for (let i = 0; i < q.length; i++) {
+//         // Store value at index 0 and continue
+//         if (lastValue === null) {
+//             lastValue = q[i];
+//             continue;
+//         }
+
+//         // Check to see if any values are more then 2 off their target index
+//         if (lastValue > i) {
+//             const individualSwaps = lastValue - i;
+//             if (individualSwaps > 2) {
+//                 tooChaotic = true;
+//                 break;
+//             }
+//         }
+//         lastValue = q[i]
+//     }
+
+//     if (tooChaotic) {
+//         console.log("Too chaotic");
+//         return;
+//     }
+    
+
+//     // If not to chaotic calculate the number of swaps which were made
+//     let sorted = false;
+//     while (!sorted) {
+//         for (let i = 0; i < q.length - 1; i++) {
+//             if (q[i] > q[i+1]) {
+//                 const tempVal = q[i]
+//                 q[i] = q[i + 1];
+//                 q[i + 1] = tempVal;
+//                 totalSwaps++;
+//                 break;
+//             }
+
+//             if (i === q.length - 2) {
+//                 sorted = true;
+//             }
+//         }
+//     }
+
+//     console.log(totalSwaps);
+// }
+
+
+// Tried to shorten it, nested loops won't run fast enough in Javascript, would need a different approach.
 const newYearChaos = (q) => {
     let totalSwaps = 0;
     let tooChaotic = false;
 
     // Check for to chaotic O(n)
-    let lastValue = null;
     for (let i = 0; i < q.length; i++) {
-        // Store value at index 0 and continue
-        if (lastValue === null) {
-            lastValue = q[i];
-            continue;
-        }
-
-        // Check to see if any values are more then 2 off their target index
-        if (lastValue > i) {
-            const individualSwaps = lastValue - i;
-            if (individualSwaps > 2) {
-                tooChaotic = true;
-                break;
+        for (let j = i + 1; j < q.length; j++) {            
+            if (q[i] > q[j]) {
+                totalSwaps++;
+                if (q[i] - (i + 1) > 2) {
+                    tooChaotic = true;
+                    break;
+                }
             }
         }
-        lastValue = q[i]
     }
 
     if (tooChaotic) {
         console.log("Too chaotic");
-        return;
+    } else {
+        console.log(totalSwaps);
     }
-    
-
-    // If not to chaotic calculate the number of swaps which were made
-    let sorted = false;
-    while (!sorted) {
-        for (let i = 0; i < q.length - 1; i++) {
-            if (q[i] > q[i+1]) {
-                const tempVal = q[i]
-                q[i] = q[i + 1];
-                q[i + 1] = tempVal;
-                totalSwaps++;
-                break;
-            }
-
-            if (i === q.length - 2) {
-                sorted = true;
-            }
-        }
-    }
-
-    console.log(totalSwaps);
 }
+
 
 
 newYearChaos([2, 1, 5, 3, 4]);
